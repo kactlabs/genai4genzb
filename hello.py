8a34225e343909ce97b5278ff59b3c3baa83369f
Raja CSP Raman <raja.csp@gmail.com>
2025-09-01 06:11:13 +0530

    Updates for 2025-09-01

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+
+
+def sayhi(limit):
+
+    for i in range(1, limit):
+
+        if i % 2 == 0:
+            print(f"{i}: hello how are you?")
+        else:
+            print(f"{i} : hello how things going?")
+
+
+# Assignment: 100, 200, 300 .. skip printing
---
+++ one.py
@@ -0,0 +1,13 @@
+
+
+import hello
+import sys
+
+limit = int(sys.argv[1]) # typecasting
+hello.sayhi(limit)
+
+"""
+How to run?
+
+py one.py 10
+"""
